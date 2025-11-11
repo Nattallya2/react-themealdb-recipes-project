c26753fc1741b6156ce33ad6c2506976c536e356
Nattallya2 <n.shmyheliuk@gmail.com>
2025-11-10 21:28:25 -0600

    2. Add video as a background, h1, add css. Add a search input and a button. All elements are centered

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import { useEffect } from 'react';
 import './App.css';
+import video from './food.mp4';
+import icon from './icons-cooking-book.png';
 
 function App() {
 
@@ -9,7 +11,6 @@ function App() {
       const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=beef`);
       const data = await response.json();
       console.log(data);
-      console.log(data.meals);
       console.log(data.meals[0].strMeal);
       console.log(data.meals[0].idMeal);
      
@@ -25,7 +26,26 @@ function App() {
 
   return (
     <div className="App">
-        Test
+
+      <div className='container'>
+        <video loop autoPlay muted>
+          <source src={video} type="video/mp4"/>
+        </video>
+        <h1>Find a Recipe</h1>
+      </div>
+
+      <div className='container'>
+          <form>
+            <input/>
+          </form>
+      </div>
+
+      <div className='container'>
+          <button>
+            <img src={icon} alt="icon"/>
+          </button>
+      </div>
+
     </div>
   );
 }
