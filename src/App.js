cc2fbffd687fc4be41d2038e105fef58a5816e78
Nattallya2 <n.shmyheliuk@gmail.com>
2025-11-10 20:58:52 -0600

    1. Request to API with useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    const getRecipe = async () => {
+      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=beef`);
+      const data = await response.json();
+      console.log(data);
+      console.log(data.meals);
+      console.log(data.meals[0].strMeal);
+      console.log(data.meals[0].idMeal);
+     
+      const recipe = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${data.meals[0].idMeal}`);
+      const recipeData = await recipe.json();
+      console.log(recipeData);
+      console.log(recipeData.meals);
+      console.log(recipeData.meals[0].strInstructions);
+      console.log(recipeData.meals[0].strSource);
+        }
+        getRecipe();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        Test
     </div>
   );
 }
